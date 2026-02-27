fc9f0337abdffd12c0f6eebeba3c959964cf1a73
oktarizha55-cpu <oktarizha55@gmail.com>
2026-02-27 10:00:19 +0700

    Add files via upload

---
+++ Latihan.py
@@ -0,0 +1,15 @@
+firstName = "Rafie"
+lastName = "Oktarizha"
+age = 22
+isMarried = False
+monthlySalary = 20000000
+
+print("My name is Rafie but people call me fio")
+print("In this year, I'm", age, "years old")
+
+def greet(name):
+    print("Hello, " + name + "! Welcome to the world of programming.")
+
+greet("Rafie")
+
+         
